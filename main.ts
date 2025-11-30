edc50323c74d0b727cca9db86d1eb81272abd783
sami-osman-2 <sami.osman@stu.ocsb.ca>
2025-11-30 00:25:19 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sami Osman
+ * Created on: Nov 2025
+ * 
 */
 
-basic.showString('Hello, World!')
+// Create a strip of 4 LEDs on pin 16
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+
+input.onButtonPressed(Button.A, function () {
+
+    // green (LED 0)
+    strip.clear()
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    strip.show()
+    basic.pause(1000)
+
+    // yellow (LED 1)
+    strip.clear()
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    strip.show()
+    basic.pause(1000)
+
+    // red (LED 2)
+    strip.clear()
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+})
+
